32990ac543454f278645f8c7915d2f874b66cc4c
Padungkiat <aomsinkaky14@gmail.com>
2022-08-18 18:21:54 +0700

    lab5_5

--- lab4q5/src/App.vue
+++ lab4q5/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <div id="flashMessage" v-if="GStore.flashMessage">
+    {{ GStore.flashMessage }}
+  </div>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -6,7 +9,24 @@
   <router-view />
 </template>
 
+<script>
+export default {
+  inject: ['GStore']
+}
+</script>
 <style>
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 7s;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,16 +34,13 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
 }
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 nav a.router-link-exact-active {
   color: #42b983;
 }
--- lab4q5/src/router/index.js
+++ lab4q5/src/router/index.js
@@ -5,6 +5,9 @@ import PassengerDetailView from '../views/event/PassengerDetailView.vue'
 import PassengerLayoutView from '../views/event/PassengerLayoutView.vue'
 import AirLineDetailView from '../views/event/AirLineDetailView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
+import NProgress from 'nprogress'
+import EventService from '@/service/EventService.js'
+import GStore from '@/store'
 const routes = [
   {
     path: '/',
@@ -25,15 +28,50 @@ const routes = [
     name: 'PassengerLayout',
     props: true,
     component: PassengerLayoutView,
+    beforeEnter: (to) => {
+      return (
+        EventService.getPassenger(to.params.id)
+          .then((response) => {
+            //Still need to set the data here
+            GStore.passenger = response.data
+          })
+          .catch((error) => {
+            if (error.response && error.response.status == 404) {
+              return {
+                name: '404Resource',
+                params: { resource: 'passenger' }
+              }
+            } else {
+              return { name: 'NetworkError' }
+            }
+          }),
+        EventService.getAirLine(to.params.id) //Return and params.id
+          .then((response) => {
+            //Still need to set the data here
+            GStore.airline = response.data
+          })
+          .catch((error) => {
+            if (error.response && error.response.status == 404) {
+              return {
+                name: '404Resource',
+                params: { resource: 'airline' }
+              }
+            } else {
+              return { name: 'NetworkError' }
+            }
+          })
+      )
+    },
+
     children: [
       {
-        path: 'passenger',
+        path: 'passenger/:id',
         name: 'PassengerDetailView',
         component: PassengerDetailView,
         props: true
       },
       {
-        path: 'airline',
+        path: 'airline/:id',
         name: 'AirLineDetailView',
         component: AirLineDetailView,
         props: true
@@ -55,7 +93,20 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
+router.beforeEach(() => {
+  NProgress.start()
+})
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
